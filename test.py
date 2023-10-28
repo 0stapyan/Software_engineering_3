7d4ff9dee0d2c2a1869edc53fe28cebfc5535e32
ostapturash <oturash@kse.org.ua>
2023-10-28 20:39:02 +0300

    Integration test for historical data for all users

--- test.py
+++ test.py
@@ -1,5 +1,7 @@
 import unittest
 from main import fetch_user_data, historical_data_for_all_users, predict_users_online, predict_user_online
+from io import StringIO
+import sys
 
 class TestIntegration(unittest.TestCase):
 
@@ -24,6 +26,21 @@ class TestIntegration(unittest.TestCase):
         self.assertTrue('willBeOnline' in prediction)
         self.assertTrue('onlineChance' in prediction)
         self.assertTrue(0 <= prediction['onlineChance'] <= 1)
+
+    def test_historical_data_for_all_users_integration(self):
+        date_str = '2023-09-27 20:00'
+
+        captured_output = StringIO()
+        sys.stdout = captured_output
+
+        historical_data_for_all_users(date_str)
+
+        printed_output = captured_output.getvalue()
+
+        sys.stdout = sys.__stdout__
+
+        expected_output = "Users Online at 2023-09-27 20:00: X\nTotal Users: Y\n"  # Replace X and Y with expected values
+        self.assertEqual(printed_output, expected_output)
 
     if __name__ == '__main__':
         unittest.main()
