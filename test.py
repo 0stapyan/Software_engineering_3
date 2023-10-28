3fe3225c38989384e8138ab3d9fd25de1e159e5e
ostapturash <oturash@kse.org.ua>
2023-10-28 20:37:27 +0300

    Integration test for predicting online concrete user

--- test.py
+++ test.py
@@ -4,16 +4,26 @@ from main import fetch_user_data, historical_data_for_all_users, predict_users_o
 class TestIntegration(unittest.TestCase):
 
     def test_predict_users_online_integration(self):
-        # Setup - You may want to create some sample data or use actual data for testing.
         future_date_str = '2023-09-28 12:00'
         user_data = fetch_user_data(0)
 
-        # Exercise - Call the function you want to test.
         prediction = predict_users_online(user_data, future_date_str)
 
-        # Assert - Check if the results match the expected outcome.
         self.assertTrue('online_users' in prediction)
         self.assertTrue(0 <= prediction['online_users'] <= len(user_data))
+
+
+    def test_predict_user_online_integration(self):
+        future_date_str = '2023-09-28 12:00'
+        user_id = 'A4DC2287-B03D-430C-92E8-02216D828709'
+        tolerance = 0.85
+        user_data = fetch_user_data(0)
+
+        prediction = predict_user_online(user_data, future_date_str, user_id, tolerance)
+
+        self.assertTrue('willBeOnline' in prediction)
+        self.assertTrue('onlineChance' in prediction)
+        self.assertTrue(0 <= prediction['onlineChance'] <= 1)
 
     if __name__ == '__main__':
         unittest.main()
